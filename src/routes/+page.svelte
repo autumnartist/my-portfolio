614bb20fc8bc3adbc16b77929add6a6230b54f56
Autumn <arartist@mit.edu>
2025-02-27 15:26:35 -0500

    img fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 
 <body style="max-width:100ch; margin-inline: max(1em, (100% - 100ch) / 2)">
     <h1>Autumn R. Artist</h1>
-    <img src="/images/stitch.jpg" alt="Stitch from Lilo and Stitch" width="500px">
+    <img src="/my-portfolio/images/stitch.jpg" alt="Stitch from Lilo and Stitch" width="500px">
     <p>I am a senior (yay!), majoring in 6-9. I live in New House and have had a great 4 years at MIT. I have 2 pets, a cat named Bruce and a dog named Cocoa. Excited for this class!</p>
     <h2>Latest Projects</h2>
     <div class="projectsHighlights">
