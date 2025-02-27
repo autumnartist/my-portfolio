71bc4b5047504e6727545770970a768e54da729c
Autumn <arartist@mit.edu>
2025-02-27 13:57:53 -0500

    Config files

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+<body style="max-width:100ch; margin-inline: max(1em, (100% - 100ch) / 2)">
+    <h1>Autumn R. Artist</h1>
+    <p>I am a senior (yay!), majoring in 6-9. I live in New House and have had a great 4 years at MIT. I have 2 pets, a cat named Bruce and a dog named Cocoa. Excited for this class!</p>
+    <img src="/images/stitch.jpg" alt="Stitch from Lilo and Stitch" width="500px">
+</body>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+  <title>Contact me</title>
+</svelte:head>
+
+<body style="max-width:100ch; margin-inline: max(1em, (100% - 100ch) / 2)">
+    <h1>Contact</h1>
+    <form class="contactFields" action="mailto:arartist@mit.edu">
+        <label class="contactLabel">
+            Subject: <input name="subject" type="text">
+        </label>
+        <label class="contactLabel">
+            Body: <textarea name="body"></textarea>
+        </label>
+        <button class="contactLabel contactSubmitButton">Submit</button>
+    </form>
+</body>
