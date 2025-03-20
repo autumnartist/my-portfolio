fd7e48513fb0632a7902ee9d4f3b9486774577f1
Autumn <arartist@mit.edu>
2025-03-20 15:07:18 -0400

    Lab 7

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,24 @@
 <script>
     import projects from "$lib/projects.json";
     import Project from "$lib/Project.svelte";
+    import { onMount } from "svelte";
 
-    let profileData = fetch("https://api.github.com/users/autumnartist");
+    // let profileData = fetch("https://api.github.com/users/autumnartist");
+
+    let githubData = null;
+    let loading = true;
+    let error = null;
+
+    onMount(async () => {
+        try {
+            const response = await fetch("https://api.github.com/users/autumnartist");
+            githubData = await response.json();
+        } catch (err) {
+            error = err;
+        }
+        loading = false;
+    });
+
 </script>
 
 <svelte:head>
@@ -14,33 +30,26 @@
     <img src="/my-portfolio/images/stitch.jpg" alt="Stitch from Lilo and Stitch" width="500px">
     <p>I am a senior (yay!), majoring in 6-9. I live in New House and have had a great 4 years at MIT. I have 2 pets, a cat named Bruce and a dog named Cocoa. Excited for this class!</p>
 
-    {#await fetch("https://api.github.com/users/autumnartist")}
-    <p>Loading...</p>
-    {:then response}
-    {#await response.json()}
-        <p>Decoding...</p>
-    {:then data}
-        <section>
-        <h2>My GitHub Stats</h2>
-        <dl class="githubStats">
-            <dt>Followers:</dt>
-            <dd>{data.followers}</dd>
-            <dt>Following:</dt>
-            <dd>{data.following}</dd>
-            <dt>Public Repositories:</dt>
-            <dd>{data.public_repos}</dd>
-            <dt>Public Gists:</dt>
-            <dd>{data.public_gists}</dd>
-            
-        </dl>
-        </section>
-    {:catch error}
+    {#if loading}
+        <p>Loading...</p>
+    {:else if error}
         <p class="error">Something went wrong: {error.message}</p>
-    {/await}
-    {:catch error}
-    <p class="error">Something went wrong: {error.message}</p>
-    {/await}
-
+    {:else}
+        <section>
+            <h2>My GitHub Stats</h2>
+            <dl class="githubStats">
+                <dt>Followers</dt>
+                <dd>{githubData.followers}</dd>
+                <dt>Following</dt>
+                <dd>{githubData.following}</dd>
+                <dt>Public Repositories</dt>
+                <dd>{githubData.public_repos}</dd>
+                <dt>Public Gists:</dt>
+                <dd>{githubData.public_gists}</dd>
+            </dl>
+        </section>
+    {/if}
+    
     <h2>Latest Projects</h2>
     <div class="projectsHighlights">
         {#each projects.slice(0, 3) as p}
