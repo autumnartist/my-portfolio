62d99a3581a0aa5d68f48948013f9cf8885d830b
Autumn <arartist@mit.edu>
2025-04-07 13:18:46 -0400

    Adding url component to projects and adding bikewatching project

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -1,25 +1,27 @@
 <script>
-    export let data = {};
-    export let hLevel = 2;
-  </script>
-  
+  export let data = {};
+  export let hLevel = 2;
+</script>
+
 <article class="indiProject">
-    <svelte:element this={"h" + hLevel}>{data.title}</svelte:element>
-    <img src={data.image} alt="">
-    <div>
-      <p>{data.description}</p>
+  <svelte:element this={"h" + hLevel}>{data.title}</svelte:element>
+  <img src={data.image} alt="" />
+  <div>
+    {#if data.url !== ""}
+      <a href={data.url} target="_blank">Visit project</a>
+    {/if}
+    <p>{data.description}</p>
     <p>{data.year}</p>
-    </div>
-    
+  </div>
 </article>
 
 <style>
-.indiProject{
-  margin: 0.5em;
-  display:grid;
-  grid-template-rows: subgrid;
-  grid-row: span 3;
-  line-height: 1.1;
-  text-wrap:balance;
-}
+  .indiProject {
+    margin: 0.5em;
+    display: grid;
+    grid-template-rows: subgrid;
+    grid-row: span 3;
+    line-height: 1.1;
+    text-wrap: balance;
+  }
 </style>
